bf903ee171f85048c9bd01e45f50ea602f40271f
Timothy Joseph Busch <timbusch1@gmail.com>
2019-07-03 13:52:01 -0700

    before class commit

--- src/main/java/com/goldbeardsea/taskmaster/controller/TaskController.java
+++ src/main/java/com/goldbeardsea/taskmaster/controller/TaskController.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class TaskController {
@@ -18,17 +20,30 @@ public class TaskController {
 
     //tasks
     @GetMapping("/tasks")
-    public String getAllTasks(Model m) {
+    public ArrayList<Task> getAllTasks(Model m) {
         ArrayList<Task> arrayList = (ArrayList<Task>) taskRepository.findAll();
-        m.addAttribute("task", arrayList);
-        return "task";
+        return arrayList;
     }
 
     @PostMapping("/tasks")
-    public void taskCreation(Model m, @RequestParam String title, @RequestParam String description) {
+    public Task taskCreation(@RequestParam String title, @RequestParam String description) {
         Task newTask = new Task(title, description, "Available");
         taskRepository.save(newTask);
-
+        return newTask;
     }
 
+    @GetMapping("/users/{name}/tasks")
+    public ArrayList<Task> getTasks(@RequestParam String name) {
+        ArrayList arrayList = (ArrayList) taskRepository.findAll();
+        // iterate across the lists and remove all non assigned name objects
+        return arrayList;
+    }
+
+    @PutMapping("/tasks/{id}/state")
+    public Task updateTask(@RequestParam String id) {
+        Optional<Task> task = taskRepository.findById(id);
+//        Grab the object advance the status as long as it can be advanced.
+//        task.get().setStatus();
+        return task.get();
+    }
 }
